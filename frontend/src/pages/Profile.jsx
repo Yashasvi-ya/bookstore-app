7df070787db829a8c27c619853cd6246702bb816
Yashasvi <yashasvi123.yy@gmail.com>
2024-10-06 23:39:16 +0530

    added profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { CgProfile } from "react-icons/cg";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div>
-      {
-        currentUser && (
-          <div>
-            <h1>{currentUser._id}</h1>
-            <h1>{currentUser.username}</h1>
-            <h1>{currentUser.email}</h1>
+      {currentUser && (
+        <div className="h-screen w-full flex flex-col justify-center items-center">
+          <div className="h-1/2 w-1/4 flex flex-col justify-center items-center">
+            <div className=" h-full w-full rounded-lg p-3 flex flex-col justify-center items-center shadow-2xl">
+              <div className="w-1/2 h-1/2 flex justify-center items-center">
+                <CgProfile className="h-full w-1/2"/>
+              </div>
+              <h1 className="text-3xl font-bold text-gray-900">
+                {currentUser.username}
+              </h1>
+              <h1>{currentUser.email}</h1>
+            </div>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
-  )
+  );
 }
